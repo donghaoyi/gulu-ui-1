2a3ae0a9292bc03dec65bb1f133c1db857d20884
donghaoyi <1203350867@qq.com>
2021-05-24 21:05:45 +0800

    switch添加流程动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -37,11 +37,15 @@ span {
   top: 2px;
   border-radius: $h2/2;
   background: white;
+  transition: left 250ms;
 }
 button.checked{
     background: blue;
 }
 button.checked>span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus{
+    outline: none;
 }
 </style>
