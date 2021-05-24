4cd6905a1c6ee22abaa6f8f08ea5a1e13cb186c8
donghaoyi <1203350867@qq.com>
2021-05-24 15:26:18 +0800

    aside：初始化、基础CSS

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,16 +2,50 @@
   <div>
     <Topnav></Topnav>
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
-export default{
-    components:{
-        Topnav
-    }
-}
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
 </script>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
