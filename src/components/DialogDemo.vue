a58e4717092522eb7c21bd04e5690ae820a4e9ad
donghaoyi <1203350867@qq.com>
2021-05-26 22:37:09 +0800

    Dialog支持 visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-       DialogDemo 文档页
-    </div>
+    <div>示例</div>
+    <h1>示例1</h1>
+    <Button @click="toggle">打开Dialog</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="true" :ok="ok" :cancal="cancal"></Dialog>
 </template>
+<script lang="ts">
+import { ref } from 'vue'
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+export default {
+    components:{
+        Dialog,
+        Button
+    },
+    setup(props) {
+        const x = ref(false);
+        const toggle=()=>{
+            x.value=!x.value;
+        }
+        const ok = ()=>{
+            return false;
+        };
+        const cancal = ()=>{};
+        return {x,toggle};
+    }
+}
+</script>
