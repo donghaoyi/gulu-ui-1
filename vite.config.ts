19e597530588d6b7e2a522a104731ad463f2c3e3
donghaoyi <1203350867@qq.com>
2021-07-21 11:01:44 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import fs = require("fs");
 import { baseParse } from '@vue/compiler-core'
 
 export default {
+  assetsDir:'assets',
+  base:'./',
   plugins: [md()],
   vueCustomBlockTransforms: {
     demo: (options) => {
