9496bc380ec0030877b22fd87e5c9e9475e820f2
donghaoyi <1203350867@qq.com>
2021-07-21 16:44:38 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,8 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button @click="toggleCode">查看代码</Button>
+      <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+      <Button @click="showCode" v-else>查看代码</Button>
     </div>
     <div class="demo-code" v-if="codeVisible">
       <pre
@@ -38,14 +39,18 @@ export default {
       );
     });
     const codeVisible = ref(false);
-    const toggleCode = () => {
-      codeVisible.value = !codeVisible.value;
+    const showCode = () => {
+      codeVisible.value = true;
+    };
+    const hideCode = () => {
+      codeVisible.value = false;
     };
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode
+      showCode,
+      hideCode
     };
   },
 };
